a7a9febbb8f6e0c352b7581dfbd2950b72cb24e2
dip_0421 <59311047+deeptika@users.noreply.github.com>
2025-01-10 15:17:24 -0500

    type path command added

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 import sys
+import os
 
 builtin_commands = ["echo", "exit", "type"]
+PATH = os.environ.get('PATH')
 
 def main():
     # Uncomment this block to pass the first stage
@@ -12,14 +14,24 @@ def main():
         match command.split():
             case ["exit", "0"]:
                 sys.exit(0)
+
             case ["echo", *args]:
                 print(*args)
+
             case ["type", *args]:
                 cmd = args[0]
+                cmd_path = None
+                paths = PATH.SPLIT(":")
+                for path in paths:
+                    if os.path.isfile(f"{path}/{cmd}"):
+                        cmd_path = f"{path}/{cmd}"
                 if cmd in builtin_commands:
                     print(f"{cmd} is a shell builtin")
+                elif cmd_path:
+                    print(f"{cmd} is {cmd_path}")
                 else:
                     print(f"{cmd}: not found")
+
             case _:
                 print(f"{command}: command not found")
 
