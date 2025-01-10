f0376314658c3f3669e35a76999eefceddeaeec8
dip_0421 <59311047+deeptika@users.noreply.github.com>
2025-01-10 15:48:22 -0500

    Support to run custom executable files

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 import sys
 import os
+import subprocess as sp
 
 builtin_commands = ["echo", "exit", "type"]
 PATH = os.environ.get('PATH')
+
+def find_path(cmd):
+    cmd_path = None
+    paths = PATH.split(":")
+    for path in paths:
+        if os.path.isfile(f"{path}/{cmd}"):
+            cmd_path = f"{path}/{cmd}"
+    return cmd_path
 
 def main():
     # Uncomment this block to pass the first stage
@@ -20,17 +29,22 @@ def main():
 
             case ["type", *args]:
                 cmd = args[0]
-                cmd_path = None
-                paths = PATH.split(":")
-                for path in paths:
-                    if os.path.isfile(f"{path}/{cmd}"):
-                        cmd_path = f"{path}/{cmd}"
+                cmd_path = find_path(cmd)
                 if cmd in builtin_commands:
                     print(f"{cmd} is a shell builtin")
                 elif cmd_path:
                     print(f"{cmd} is {cmd_path}")
                 else:
                     print(f"{cmd}: not found")
+
+            case [*args]:
+                cmd = args[0]
+                cmd_path = find_path(cmd)
+                if cmd_path:
+                    # execute cmd and " ".join(args)
+                    sp.run(args=args, capture_output=True, text=True)
+                else:
+                    print(f"{command}: command not found")
 
             case _:
                 print(f"{command}: command not found")
