078ed23e03eca3d8d6b175e2d8d906cf47cfd334
dip_0421 <59311047+deeptika@users.noreply.github.com>
2025-01-10 00:55:52 -0500

    Echo command support added: bug fix #5

--- app/main.py
+++ app/main.py
@@ -3,18 +3,18 @@ import sys
 
 def main():
     # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
+    while True:
+        sys.stdout.write("$ ")
 
-    # Wait for user input
-    command = input()
-    while command:
-        if command == "exit 0":
-            sys.exit(0)
-        if command.split()[0] == "echo":
-            print(f"{command[5:]}")
-        else:
-            print(f"{command}: command not found")
-        main()
+        command = input()
+
+        match command.split():
+            case ["exit", "0"]:
+                sys.exit(0)
+            case ["echo", *args]:
+                print(*args)
+            case _:
+                print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
