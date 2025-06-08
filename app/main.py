0d892c5926a99ff4f8a7b51478c7916a6af369d0
dip_0421 <59311047+deeptika@users.noreply.github.com>
2025-06-07 20:56:18 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-import sys
 import os
-import subprocess as sp
 import shlex
+import subprocess as sp
+import sys
 
 BUILTINS = ["echo", "exit", "type", "pwd", "cd"]
 PATH = os.environ.get('PATH')
@@ -9,49 +9,49 @@ HOME = os.environ.get('HOME')
 
 
 def find_path(cmd):
-    cmd_path = None
-    paths = PATH.split(":")
-    for path in paths:
-        if os.path.isfile(f"{path}/{cmd}"):
-            cmd_path = f"{path}/{cmd}"
-    return cmd_path
+    for path in PATH.split(":"):
+        full_path = os.path.join(path, cmd)
+        if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+            return full_path
+    return None
 
 
 def process_command(input_txt):
-    _ = input_txt.split("'")
-    return [x for x in _ if x.strip() != '']
+    return shlex.split(input_txt)
 
 
 def main():
     while True:
         sys.stdout.write("$ ")
+        sys.stdout.flush()
+        try:
+            command_line = input()
+        except EOFError:
+            break
+        if not command_line.strip():
+            continue
+        tokens = shlex.split(command_line)
+        command = tokens[0]
+        args = tokens[1:]
 
-        command_txt = input()
-        command, *args = command_txt.split(" ")
-
-        if command == 'exit' and args[0] == '0':
-            sys.exit(0)
+        if command == 'exit':
+            code = int(args[0]) if args else 0
+            sys.exit(code)
 
         if command == 'echo':
-            echo_txt = command_txt[5:]
-            if echo_txt.startswith("'") and echo_txt.endswith("'"):
-                echo_txt = echo_txt[1:-1]
-                echo_txt = "".join(echo_txt.split("'"))
-                print(echo_txt)
-            else:
-                print(" ".join(shlex.split(echo_txt)))
+            print(" ".join(args))
 
         elif command == 'pwd':
             print(os.getcwd())
 
         elif command == 'cd':
             try:
-                if args[0] == '~':
+                if not args or args[0] == '~':
                     os.chdir(HOME)
                 else:
-                    os.chdir(*args)
+                    os.chdir(args[0])
             except FileNotFoundError:
-                print(f"{command}: {args[0]}: No such file or directory")
+                print(f"{command}: {args[0] if args else ''}: No such file or directory")
 
         elif command == 'type':
             cmd = args[0]
@@ -63,11 +63,15 @@ def main():
                 print(f"{cmd}: not found")
 
         else:
-            if find_path(command):
-                processed_args = process_command(command_txt[len(command) + 1:])
-                sp.run(args=[command, *processed_args])
+            cmd_path = find_path(command)
+            if cmd_path:
+                try:
+                    sp.run([command] + args)
+                except Exception as e:
+                    print(f"Error running command: {e}")
             else:
                 print(f"{command}: command not found")
+
 
 
 if __name__ == "__main__":
